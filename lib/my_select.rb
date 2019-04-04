20da1be1c2cc9c8f8756abed8c70224bca2e2f61
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-04 00:42:41 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0
   new_array =[]
   while i < collection.length
-    if yield)collection[i]
+    if yield(collection[i])
       new_array << collection[i]
         i += 1
     end
